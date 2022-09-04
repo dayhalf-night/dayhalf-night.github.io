f8ad3dc5bc9c320a677804289d8ee50d8e0344cf
dayhalf-night <2409805740@qq.com>
2022-09-04 12:16:59 +0800

    first try

---
+++ src/components/calculate.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+import { NButton, NCard, NSpace } from 'naive-ui';
+import { defineComponent } from 'vue';
+import abstract from './abstract.vue';
+import news from './news.vue';
+export default defineComponent({
+    components: {
+    abstract,
+    news,
+    NSpace,
+    NCard,
+    NButton
+},
+setup(){
+    const turnTo = () => {
+        window.location.href="#textAnalysis"
+    }
+    return {
+        turnTo
+    }
+}
+})
+</script>
+
+<template>
+    <n-space>
+        <news />
+        <n-space vertical>
+            <abstract />
+            <n-card :bordered="false" title="预测结果">
+                真<br />
+                模型预测准确率：50%<br />
+                <template #footer>
+                    <n-button text @click.prevent="turnTo">
+                        点击查看可视化视图
+                    </n-button>
+                </template>
+            </n-card>
+        </n-space>
+    </n-space>
+</template>
---
+++ src/components/textanalysis.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { NCard } from 'naive-ui';
+import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        components:{
+            NCard
+        }
+    })
+</script>
+
+<template>
+    <n-card title="关键词可视化">
+        <img src="../assets/wordcloud.png" style="height:80%">
+    </n-card>
+</template>
+
+<style scoped>
+    .n-card{
+        max-height: 500px;
+    }
+</style>
